015ef450ffe72687849de2f79e208fd558e69be4
Nathan Pho <80579519+nathan6am@users.noreply.github.com>
2022-06-08 16:13:20 -0400

    error handling for game start

--- handlers/GameHandler.js
+++ handlers/GameHandler.js
@@ -3,27 +3,32 @@ const { updateGame } = require("../services/GameManager");
 
 module.exports = (io, socket) => {
   const startGame = (lobbyId, callback) => {
-    let updatedLobby = lobbyManager.startCurrentGame(lobbyId, socket.id);
-    const game = updatedLobby.game;
-    if (!updatedLobby) {
-      callback(false);
-    } else {
-      callback(true);
-      io.to(lobbyId).emit("lobby:update", updatedLobby);
-      if (game.options.timeLimit) {
-        setTimeout(() => {
-          const lobby = lobbyManager.getLobby(lobbyId);
-          lobby.gameActive = false;
-          let gameState = lobbyManager.getLobbyGameState(lobbyId);
-          gameState.isOver = true;
-          lobby.game = gameState;
-          const users = lobby.users.map((user) => {
-            return { ...user, ready: false };
-          });
-          lobby.users = users;
-          io.to(lobbyId).emit("game:ended", lobby);
-        }, (game.options.timeLimit + 10) * 1000);
+    try {
+      let updatedLobby = lobbyManager.startCurrentGame(lobbyId, socket.id);
+      const game = updatedLobby.game;
+      if (!updatedLobby) {
+        callback(false);
+      } else {
+        callback(true);
+        io.to(lobbyId).emit("lobby:update", updatedLobby);
+        if (game.options.timeLimit) {
+          setTimeout(() => {
+            const lobby = lobbyManager.getLobby(lobbyId);
+            lobby.gameActive = false;
+            let gameState = lobbyManager.getLobbyGameState(lobbyId);
+            gameState.isOver = true;
+            lobby.game = gameState;
+            const users = lobby.users.map((user) => {
+              return { ...user, ready: false };
+            });
+            lobby.users = users;
+            io.to(lobbyId).emit("game:ended", lobby);
+          }, (game.options.timeLimit + 10) * 1000);
+        }
       }
+    } catch (e) {
+      console.error(e.message);
+      callback(false);
     }
   };
 
