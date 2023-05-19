ccec34cb4c2ebeccf775a9d94d092be96a0f99b7
nathan6am <nathantpho@gmail.com>
2023-05-19 13:12:32 -0400

    https support

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const { createServer } = require("http");
+const https = require("https");
 const { Server } = require("socket.io");
+const fs = require("fs");
+require("dotenv").config();
 
+const environment = process.env.NODE_ENV || "development";
+const credentials =
+  environment === "development"
+    ? null
+    : {
+        key: fs.readFileSync(process.env.KEY_PATH, "utf-8"),
+        cert: fs.readFileSync(process.env.CERT_PATH, "utf-8"),
+      };
+console.log(credentials);
 const app = express();
-const httpServer = createServer(app);
+const httpServer = environment === "development" ? createServer(app) : https.createServer(credentials, app);
 const registerLobbyHandlers = require("./handlers/LobbyHandler.js");
 const registerGameHandlers = require("./handlers/GameHandler.js");
 const registerDisconnectHandler = require("./handlers/DisconnectHandler.js");
-const port = process.env.PORT || 3000;
+const port = environment === "development" ? process.env.DEV_PORT || 8000 : process.env.PROD_PORT || 443;
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
